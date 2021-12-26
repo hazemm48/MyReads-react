c2078d71ddd6a25cc7387e0cc2dc1032ddcff0fd
Hazem Mohamed <hazem1abc@gmail.com>
2021-12-26 20:50:31 +0200

    Update Grid.js

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,14 +4,17 @@ import Menu from './Books'
 class Grid extends Component{
 render(){
     return(<div>
-        <li key={this.props.book.id} value={this.props.book.id}>
+        <li value={this.props.book.id}>
         <div className="book" >
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${this.props.book.imageLinks.thumbnail})` }}></div>
+            <div className="book-cover" style={{
+              width: 128, height: 193, 
+              backgroundImage:`url(${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : '' })`}}>
+              </div>
             <Menu moveBook={this.props.moveBook} shelf={this.props.book.shelf} />
           </div>
-          <div className="book-title">{this.props.book.title}</div>
-          <div className="book-authors">{this.props.book.authors}</div>
+          <div className="book-title">{this.props.book.title ? this.props.book.title : 'No title'}</div>
+          <div className="book-authors">{this.props.book.authors ? this.props.book.authors: 'No authors' }</div>
         </div>
       </li>
 </div>)
