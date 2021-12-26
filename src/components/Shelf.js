e2b7367c38d6c502acbf9a2f8e5b1c158e04f559
Hazem Mohamed <hazem1abc@gmail.com>
2021-12-26 20:53:08 +0200

    Update Shelf.js

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -9,7 +9,7 @@ class Shelfs extends Component{
               <h2 className="bookshelf-title">{this.props.title}</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                {this.props.books && this.props.books.map( (book) => (<Grid book={book} moveBook={this.props.moveBook} books={this.props.books}/>))}
+                {this.props.books && this.props.books.map( (book) => (<Grid book={book} moveBook={this.props.moveBook} books={this.props.books} key={book.id}/>))}
                 </ol>
               </div>
             </div>
